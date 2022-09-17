9dfc23904ee896c1534943293e9c064b77a52e04
Alexander Karpinets <alexanderkarpinets@gmail.com>
2022-09-17 17:33:41 +0300

    Initial Commit

---
+++ RandomPersonList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPersonList
+//
+//  Created by Karpinets Alexander on 17.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
